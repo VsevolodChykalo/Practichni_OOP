51c09d40445cad21abc0e17a59607abdc99e661b
VsevolodChykalo <114733624+VsevolodChykalo@users.noreply.github.com>
2024-06-27 17:47:19 +0300

    Add files via upload

---
+++ PR_5/PR_5/SharpLabs2024/Mages/FireMage.cs
@@ -0,0 +1,7 @@
+﻿namespace CSharpLabs2024.Mages
+{
+    public class FireMage : Mage
+    {
+        public FireMage(string name, int hp, int mana) : base(name, hp, mana) { }
+    }
+}
---
+++ PR_5/PR_5/SharpLabs2024/Spells/WaterGun.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpLabs2024.Spells
+{
+    public class WaterGun : ISpell
+    {
+        public int Damage { get; } = 25;
+        public int ManaCost { get; } = 8;
+
+        public void CastSpell(IDamageble target, Mage caster)
+        {
+            target.TakeDamage(Damage);
+        }
+    }
+}
